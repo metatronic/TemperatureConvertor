b1b1fd4c3ca15944bfe7d545465770af69e6ef69
pran saha <iampransaha@gmail.com>
2021-09-02 21:23:27 +0530

    Add project files.

---
+++ TemperatureConvertor/Temperature.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TemperatureConvertor
+{
+    internal class Temperature : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private TemperatureCommand celciusToFarenhietCommand = null;
+        public ICommand CelciusToFarenhietCommand
+        {
+            get { return celciusToFarenhietCommand; }
+        }
+        private TemperatureCommand farenhietToCelciusCommand = null;
+        public ICommand FarenhietToCelciusCommand
+        {
+            get { return farenhietToCelciusCommand; }
+        }
+        public Temperature()
+        {
+            celciusToFarenhietCommand = new TemperatureCommand(CelciusToFarenhiet);
+            farenhietToCelciusCommand = new TemperatureCommand(FarenhietToCelcius);
+        }
+        public double TemperatureValue { get; set; }
+        private double convertedTemperatureValue;
+        public double ConvertedTemperatureValue
+        {
+            get { return convertedTemperatureValue; }
+            set { convertedTemperatureValue = value; PropertyChanged(this, new PropertyChangedEventArgs("ConvertedTemperatureValue")); }
+        }
+        public void CelciusToFarenhiet(object obj)
+        {
+            ConvertedTemperatureValue = (TemperatureValue * 9 / 5) + 32;
+        }
+        public void FarenhietToCelcius(object obj)
+        {
+            ConvertedTemperatureValue = (TemperatureValue - 32) * 5 / 9;
+        }
+    }
+}
---
+++ TemperatureConvertor/TemperatureCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TemperatureConvertor
+{
+    class TemperatureCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public TemperatureCommand(Action<object> execute) : this(execute, null)
+        {
+
+        }
+        public TemperatureCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
